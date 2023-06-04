ad5101bb2b094546ad1ef1b9417b3c85dd30f421
matan <matankaufman1@gmail.com>
2023-06-04 13:20:36 +0300

    nexus repo add loguru

--- nexus/setup.py
+++ nexus/setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup, find_packages
 
-pyTelegramBotAPI_setup_args = dict(
-    name='pyTelegramBotAPI',
-    version='1.0.0',
-    description='pyTelegramBotAPI',
+loguru_setup_args = dict(
+    name='loguru',
+    version='0.7.0',
+    description='loguru',
     license='MIT',
     install_requires=[
         'yt-dlp>=2023.2.17',
         'pyTelegramBotAPI',
         'boto3',
         'loguru',
+        'python>=3.5',
     ],
     author='Matt',
     author_email='example@example.com'
 )
 
 if __name__ == '__main__':
-    setup(**pyTelegramBotAPI_setup_args)
+    setup(**loguru_setup_args)
 
 
