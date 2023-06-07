2e338d712d00ef758479b27d64f3c236849aba5b
matan <matankaufman1@gmail.com>
2023-06-07 14:43:28 +0300

    nexus-host fixed

--- nexus/setup.py
+++ nexus/setup.py
@@ -1,22 +1,19 @@
 from setuptools import setup, find_packages
 
-pyTelegramBotAPI_setup_args = dict(
-    name='pyTelegramBotAPI',
-    version='4.12.0',
-    description='pyTelegramBotAPI',
-    license='GPL2',
+pypi_setup_args = dict(
+    name='pypi',
+    version='1.0.0',
+    description='pypi',
+    license='MIT',
     install_requires=[
-        'yt-dlp>=2023.2.17',
-        'pyTelegramBotAPI',
-        'boto3',
-        'loguru',
-        'python>=3.5',
+
+
     ],
     author='Matt',
     author_email='example@example.com'
 )
 
 if __name__ == '__main__':
-    setup(**pyTelegramBotAPI_setup_args)
+    setup(**pypi_setup_args)
 
 
